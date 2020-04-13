8062471233f638d17e6c4202632544ef14db88e6
xz80100 <xz80100@yahoo.com.tw>
2020-04-13 23:55:00 +0800

    weather data

--- api.py
+++ api.py
@@ -1,3 +1,40 @@
-import requests
-x = 5
-print(x)
+import requests  #直接用alt+enter安裝套件
+from data import Location, WeatherElement
+url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=rdec-key-123-45678-011121314'
+html_content = requests.get(url)    #向html提出get請求
+
+json_content = html_content.json()    #將內容轉為json
+print(json_content)
+
+#取得測站資料
+records = json_content.get('records')
+location = records.get('location') #從location裡面拿取資料
+
+#取得欄位資料，location是list，解析這個資料
+for l in location:  #跑location裡面的資料，會得出dictionary資料
+    #lat = l.get('lat')
+    #lon = l.get('lon')
+    #locationName = l.get('locationName')
+    #stationId = l.get('stationId')
+    #time = l.get('time').get('obsTime')
+    #print(locationName, stationId, time)
+
+    a = Location()
+    a.from_json(l)
+    #a.weatherelement.humd(有問題)
+    print(a.__dict__)
+
+
+
+
+
+#取得觀測資料，weatherElement是list
+weatherElement = l.get('weatherElement')
+for element in weatherElement:
+    elementName = element.get('elementName')
+    elementValue = element.get('elementValue')
+    print(elementName, elementValue)
+
+
+
+
