79fdcdd84fe57fe4f7a9837ab7a645eaa123b27d
xz80100 <xz80100@yahoo.com.tw>
2020-04-16 20:57:24 +0800

    parse-dcard

--- parse_dcard.py
+++ parse_dcard.py
@@ -10,7 +10,7 @@ driver.get(url)
 
 #在搜尋的輸入框內輸入文字
 inputElement = driver.find_element_by_tag_name('input') #找到輸入框
-inputElement.send_keys('python')  #輸入要搜尋的關鍵字
+inputElement.send_keys('tensorflow')  #輸入要搜尋的關鍵字
 
 #點擊搜尋按鈕
 driver.find_element_by_css_selector('button.sc-1ehu1w3-2').click() #找到符合要搜尋的元件名稱
@@ -28,9 +28,15 @@ print(meta_datas)
 
 #從meta_datas裡面挑出看板
 forums = []
+author = []
+times = []
 for i in range(len(meta_datas)):
     if i % 3 == 0:
         forums.append(meta_datas[i])
+    if i % 3 == 1:
+        author.append(meta_datas[i])
+    if i % 3 == 2:
+        times.append(meta_datas[i])
 
 #獲得文章標題
 titles = []
@@ -50,4 +56,5 @@ for href in hrefs:
 print(links)
 
 for i in range(len(forums)):
-    print(forums[i], titles[i], links[i])
+    if forums[i] == '軟體工程師':
+        print(forums[i], titles[i], links[i])
